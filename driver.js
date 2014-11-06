4db677416bc3ccb783376a50ef39b929ac525f5f
at1as <jason@willems.ca>
2014-11-06 17:35:38 -0500

    Login parameters for site

--- driver.js
+++ driver.js
@@ -4,6 +4,7 @@ var async     = require("async");
 var webdriver = require("selenium-webdriver");
 var PNGDiff   = require("png-diff");
 var colors    = require("colors");
+var sleep     = require("sleep")
 
 var setup     = require(__dirname + "/setup");
 var render    = require(__dirname + "/render");
@@ -23,34 +24,61 @@ function run(body, done) {
   var status        = {'pass_count':0, 'fail_count':0, 'na_count':0, 'error_count':0, 'resolved_count':0, 'total':0}
   var test_started  = false;
 
-  // Retrieve and validate URI list
-  /*
-  try {
-    var site_list = require(__dirname + "/sites.json");
-    var uri_list  = JSON.parse(JSON.stringify(site_list));
-  } catch (e) {
-    console.error('\nsites.json is not valid JSON formatting: '.red + e + '\n');
-    process.exit(1);
+
+  // Ensure all browser screenshots are same size
+  var CHROME_OFFSET_H = 108;
+  if (body.browser == "chrome") {
+    var browser_height  = body.height + CHROME_OFFSET_H;
+    var browser_width   = body.width;
+  } else {
+    var browser_height  = body.height;
+    var browser_width   = body.width;
   }
-  */
 
 
   // Spawn Driver
-  var driver = new webdriver.Builder().
-      withCapabilities(capabilities).
-      build();
-  driver.manage().window().setSize(parseInt(body.width), parseInt(body.height));
+  var driver =  new webdriver.Builder().
+                withCapabilities(capabilities).
+                build();
+
+  driver.manage().window().setSize(parseInt(browser_width), parseInt(browser_height));
+
+  // Login iff login parameters specified
+  if (body.login_enabled) {
+    driver.get(body.protocol + body.url + body.login_urn).then(function() {
+    driver.sleep(1000);
+    driver.findElement(webdriver.By.name(body.usr_val)).sendKeys(body.usr).then(function() {
+      driver.sleep(1000);
+      driver.findElement(webdriver.By.name(body.pwd_val)).sendKeys(body.pwd).then(function() {
+          driver.sleep(1000);
+          driver.findElement(webdriver.By.id(body.login_btn)).click().then(function() {
+              testRunner();
+          });
+      });
+    });
+  });
+  } else {
+    testRunner();
+  }
 
 
   // Loop through each URI
-  body.urn.forEach( function(i) {
-    collectScreen(i);
-  });
+  function testRunner() {
+    body.urn.forEach( function(i) {
+      collectScreen(i, renderResults);
+    });
+  }
 
 
   // Generate final report only after all screens have been compared
   function renderResults() {
+
+    console.log("renderResults() called");
+
     if (waiting == 0 && test_started == true) {
+
+      console.log("Test Complete");
+
       driver.quit();
       var timestamp = (new Date).toISOString();
       status.total = status.pass_count + status.fail_count + status.na_count + status.error_count;
@@ -67,7 +95,9 @@ function run(body, done) {
 
 
   // Collect and Compare screens
-  function collectScreen(urn) {
+  function collectScreen(urn, renderResults) { //renderResults()
+
+    console.log("collectScreens() called")
 
     waiting ++;
     test_started = true;
@@ -82,13 +112,23 @@ function run(body, done) {
       var filepath = __dirname + "/public/assets/images/screenshots/new/";
       fs.writeFileSync(filepath + filename, data, 'base64');
       console.log("\nCapturing latest screens from: \n\t" + uri.green);
+      //console.log("ScreenShot Taken");
     }
 
     // Navigate Driver to URL and compare screenshots
-    driver.get(uri);
-    driver.takeScreenshot().then(function(data, done) {
+    driver.get(uri).then(function() {
+      driver.sleep(1000);
+    });
+
+    driver.takeScreenshot().then(function(data) {
 
       writeScreenshot(data, uri_safe + '.png');
+
+      // TODO: Phantonjs is too fast. Compares screenshots before they're written
+      // Remove this sleep and make writeScreenshot blocking for all browsers
+      if (body.browser == "phantomjs") {
+        sleep.sleep(2);
+      }
 
       var prev_dir  = __dirname + '/public/assets/images/screenshots/old/';
       var curr_dir  = __dirname + '/public/assets/images/screenshots/new/';
@@ -114,7 +154,7 @@ function run(body, done) {
               results.push(testResult);
               status.error_count ++;
               waiting --;
-              //renderResults();
+              renderResults();
             } else {
               console.log('Comparing: \n\t' + old_screenshot.yellow + ' with ' + new_screenshot.yellow);
               if (diffMetric === 1) {
@@ -124,15 +164,16 @@ function run(body, done) {
                 results.push(testResult);
                 status.fail_count ++;
                 waiting --;
+                renderResults();
               } else {
                 console.log("Result: \n\t" + "No difference.\n".green);
                 testResult.result = 'PASS';
                 results.push(testResult);
                 status.pass_count ++;
                 waiting --;
+                renderResults();
               }
             }
-            renderResults();
           });
         } else {
           console.log("\nNo older screen to compare to.".yellow + " Setting this screen to master for the next run\n");
