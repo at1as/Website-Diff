c6d4ba9d67c3649fbfa071f29a383814f6b12524
at1as <jason@willems.ca>
2014-11-18 22:24:31 -0500

    Create per browser screenshot folder

--- setup.js
+++ setup.js
@@ -10,7 +10,7 @@ function createDir(directory) {
 }
 
 function createFile(filename, content) {
-  if (!fs.exists(filename)) {
+  if (!fs.existsSync(filename)) {
     fs.writeFile(filename, content, function(err){
       if(err) console.log(err);
     });
@@ -25,10 +25,24 @@ function createDirs() {
   createDir('./public/assets/images/screenshots/new');
   createDir('./public/assets/images/screenshots/old');
   createDir('./public/assets/images/screenshots/results');
+  if (fs.existsSync('./browser-list.json')) {
+    var browsers = JSON.parse(fs.readFileSync('./browser-list.json'));
+    var browser_list = browsers.browsers;
+    for(i=0; i<browser_list.length; i++){
+      screensDir(browser_list[i]);
+    }
+  }
 }
 
 function createFiles() {
-  createFile('executions.json', JSON.stringify({"log" : []}));
+  createFile('./executions.json', JSON.stringify({"log" : []}));
+  createFile('./browser-list.json', JSON.stringify({"browsers" : []}));
+}
+
+function screensDir(browser){
+  createDir('./public/assets/images/screenshots/new/' + browser);
+  createDir('./public/assets/images/screenshots/old/' + browser);
+  createDir('./public/assets/images/screenshots/results/' + browser);
 }
 
 module.exports.directory    = createDir;
@@ -36,3 +50,5 @@ module.exports.file         = createFile;
 
 module.exports.directories  = createDirs;
 module.exports.files        = createFiles;
+
+module.exports.screensDir   = screensDir;
